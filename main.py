4eb22a06e740e894cef8a0d0d8cfec761f089502
maximusspolini-crpusd <maximus.spolini@crpusd.org>
2026-05-15 09:43:38 -0700

    a

--- main.py
+++ main.py
@@ -186,7 +186,7 @@ load_level(current_level)
 # Load AI
 print("Loading AI Brain...")
 try:
-    ai_brain = PPO.load("brain4")
+    ai_brain = PPO.load("brain5")
     print("AI Brain Loaded Successfully!")
 except:
     print("Warning: ai_brain.zip not found. Manual play only.")
